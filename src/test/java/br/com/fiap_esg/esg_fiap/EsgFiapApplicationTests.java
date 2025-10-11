90cce18cf55ace9ddb9e062503cad445ab86880e
anderson-1999 <anderson.silvadealmeida1999@gmail.com>
2025-10-11 08:49:34 -0300

    configurando o mavem com perfiel de teste no h2

--- src/test/java/br/com/fiap_esg/esg_fiap/EsgFiapApplicationTests.java
+++ src/test/java/br/com/fiap_esg/esg_fiap/EsgFiapApplicationTests.java
@@ -3,10 +3,12 @@ package br.com.fiap_esg.esg_fiap;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class EsgFiapApplicationTests {
 
 	@Test
